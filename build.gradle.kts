f15bb622e17d488894db1a5ce825502aa2616bb2
Konsterna <2751222+konsterna@users.noreply.github.com>
2021-08-07 02:22:06 +0900

    Set up to collect test coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.springframework.boot") version "2.5.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.sonarqube") version "3.3"
+    id("jacoco")
     kotlin("jvm") version "1.5.21"
     kotlin("plugin.spring") version "1.5.21"
 }
@@ -16,11 +17,14 @@ repositories {
     mavenCentral()
 }
 
+val jacocoOutputLocation = file("${buildDir}/reports/jacoco.xml")
+
 sonarqube {
     properties {
         property("sonar.projectKey", "konsterna_reco")
         property("sonar.organization", "konsterna")
         property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.jacoco.xmlReportPaths", jacocoOutputLocation)
     }
 }
 
@@ -44,4 +48,15 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        xml.outputLocation.set(jacocoOutputLocation)
+    }
 }
